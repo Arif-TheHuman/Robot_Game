9c2962b77752c61014b8b2ad9495835833321745
Arif-TheHuman <22FTT2520@student.pb.edu.bn>
2024-03-15 10:56:00 +0800

    Changed to a 32x48 sprite for characters

--- GameObject.js
+++ GameObject.js
@@ -7,7 +7,7 @@ class GameObject {
     this.direction = config.direction || "down";
     this.sprite = new Sprite({
       gameObject: this,
-      src: config.src || "/images/characters/people/arifHero.png",
+      src: config.src || "/images/characters/people/test.png",
     });
 
     this.behaviorLoop = config.behaviorLoop || [];
